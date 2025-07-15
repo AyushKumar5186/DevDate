a3f9eae1c2db74d49026f4f59eb22a9361fb9874
Ayush kumar <krayush294@gmail.com>
2025-07-15 09:11:13 +0530

    signup route done

---
+++ backend/src/routes/authRoutes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const { signup } = require("../controllers/UserController");
+
+const router = express.Router();
+
+
+router.post("/signup", signup);
+
+module.exports = router;
